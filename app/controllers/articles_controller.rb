fd5c7b95238db1061f73f250f927481454adb6a0
Gavin <gavdelbo@gmail.com>
2016-05-04 23:04:43 +0000

    Completed authenication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
-    
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]  
   def new
     @article = Article.new  
   end    
@@ -14,7 +15,7 @@ class ArticlesController < ApplicationController
     #debugger
     
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -53,5 +54,12 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
-     
+ 
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end  
+    end
+  
 end    
